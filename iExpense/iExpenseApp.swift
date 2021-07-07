8df59b2b411f44929480f148fe29454d688f5fe7
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-07-07 09:15:09 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Soumyattam Dey on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
